95c87f12d0e671a10fa6b7f586cf928010a0d388
Siddhant Vairagade <87441404+Siddhant-vairagade@users.noreply.github.com>
2021-07-15 00:03:15 +0530

    Update projectoperation.js

--- projectoperation.js
+++ projectoperation.js
@@ -1,20 +1,16 @@
-// document.getElementsByClassName('close').addEventListener('click',menuclose)
-
-// function menuclose() {
-//    if()
-// }
-
-
 var sindex=1;
 var s=document.getElementsByClassName('slide');
 var i;
 var c=s.length;
 
 function load(){
+    
     s[sindex-1].style.display="block";
+
 }
 
 function goslide(n){
+
     slider(sindex += n )
 
 }
@@ -36,4 +32,29 @@ function slider(n)
 
     s[sindex-1].style.display="block";
 
+}
+
+
+
+
+const opennav=document.querySelector('.hat');
+const closenav=document.querySelector('.close');
+const menu=document.querySelector('.menu');
+
+const lposition=menu.getBoundingClientRect().left;
+
+opennav.addEventListener('click',a);
+function a(){
+    if(lposition<0)
+    {
+        menu.classList.add("show");
+    }
+}
+
+closenav.addEventListener('click',b);
+function b(){
+    if(lposition<0)
+    {
+        menu.classList.remove("show");
+    }
 }
